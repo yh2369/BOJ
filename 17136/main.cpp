4160c5ea7632a80a3a2dfc262af3b5403f01dafd
Guest <guest-kkab9g@h21460.sgb.skir.kr>
2022-07-24 12:51:41 +0900

    17136

--- 17136/main.cpp
+++ 17136/main.cpp
@@ -1,153 +1,88 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
-int min3(int a, int b, int c);
-void print2DV(vector<vector<int>>& v, int N, int M);
+const int MAX = 10;
 
-int min3(int a, int b, int c)
+int min_ans, total_cnt;
+int paper_cnt[5] = { 0,0,0,0,0 };
+int board[MAX][MAX];
+
+bool can_attach_paper(int x, int y, int size)
 {
-    int ret = a;
-    if( ret > b)
-    {
-        ret = b;
-    }
-    if(ret > c)
-    {
-        ret = c;
-    }
-    return ret;
+	register int i, j;
+	for (i = 0; i < size; ++i)
+	{
+		for (j = 0; j < size; ++j)
+			if (!board[x + i][y + j])
+				return false;
+	}
+	return true;
 }
 
-void print2DV(vector<vector<int>>& v, int N, int M)
+void update_paper(int x, int y, int size, bool is_attach)
 {
-    for(int i = 0; i<N; i++)
-    {
-        for(int j = 0; j<M; j++)
-        {
-            cout << v[i][j] << " ";
-        }
-        cout << '\n';
-    }
+	register int i, j;
+	for (i = 0; i < size; ++i)
+		for (j = 0; j < size; ++j)
+				board[x + i][y + j] = is_attach;
+}
+
+void dfs(int x, int y, int attach_cnt)
+{
+	while (board[x][y] == 0)
+	{
+		if (++y >= MAX)
+		{
+			if (++x >= MAX)
+			{
+				min_ans = min(min_ans, attach_cnt);
+				return;
+			}
+			y = 0;
+		}
+	}
+
+	if (min_ans <= attach_cnt)
+		return;
+
+	for (int s = 5; s > 0; --s)
+	{
+		if (x + s > MAX || y + s > MAX || paper_cnt[s] == 5) continue;
+
+		if (can_attach_paper(x, y, s))
+		{
+			update_paper(x, y, s, 0);
+			++paper_cnt[s]; 
+			dfs(x, y, attach_cnt + 1); 
+
+			update_paper(x, y, s, 1);
+			--paper_cnt[s];
+		}
+	}
 }
 
 int main()
 {
-
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
-    vector<vector<int>> grid(10, vector<int>(10, 0));
-    vector<vector<int>> dp(10, vector<int>(10, 0));
-    vector<vector<bool>> checking_v(10, vector<bool>(10, false));
-    bool all_zero = true;
-    for(int i = 0; i< 10; i++)
-    {
-        for(int j =0; j< 10; j++)
-        {
-            cin >> grid[i][j];
-            if(grid[i][j] != 0)
-            {
-                all_zero = false;
-            }
-        }
-    }
-    if(all_zero)
-    {
-        cout << 0 << '\n';
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	min_ans = 987654321;
+	register int i, j;
+	for (i = 0; i < MAX; ++i)
+	{
+		for (j = 0; j < MAX; ++j)
+		{
+			cin >> board[i][j];
+			if (board[i][j]) ++total_cnt;
+		}
+	}
+	if (total_cnt == MAX * MAX)
+	{
+		cout << 4; 
         return 0;
-    }
+	}
 
-    for(int j = 0; j<10; j++)
-    {
-        dp[0][j] = grid[0][j];
-    }
-
-    for(int i = 1; i<10; i++)
-    {
-        dp[i][0] = grid[i][0];
-    }
-    
-    
-    for(int i = 1; i<10; i++)
-    {
-        for(int j= 1; j<10; j++)
-        {
-            if(grid[i][j] == 1)
-            {
-                dp[i][j] = min3(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) % 5 + 1;
-            }
-        }
-    }
-
-    //print2DV(dp, 10, 10);
-    int cnt = 0;
-
-    vector<int> left_v(6, 5);
-    for(int i = 9; i>=0; i--)
-    {
-        for(int j = 9; j>=0; j--)
-        {
-            if(checking_v[i][j] == true)
-            {
-                continue;
-            }
-
-            int len  = dp[i][j];
-            int k; bool f = false;
-            for(k = len; k>0; k--)
-            {
-                for(int a = 0; a<k; a++)
-                {
-                    for(int b = 0; b<k; b++)
-                    {
-                        if(checking_v[i-a][j-b] == true)
-                        {
-                            f = true;
-                            break;
-                        }
-                    }
-                    if(f == true)
-                    {
-                        break;
-                    }
-                }
-            }
-            len = k;
-
-            bool flag = false;
-            for(int k = len; k>0; k--)
-            {
-                if(left_v[k] > 0)
-                {
-                    left_v[k]--;
-                    for(int a = 0; a<k; a++)
-                    {
-                        for(int b = 0; b<k; b++)
-                        {
-                            checking_v[i-a][j-b] = true;
-                        } 
-                    }
-                    cnt++;
-                    break;    
-                }
-                else
-                {
-                    flag = true;
-                }
-            }
-
-            if(flag)
-            {
-                cout << -1 << '\n';
-                return 0;
-            }
-            
-        }
-    }
-
-
-
-    cout << cnt << '\n';
-    return 0;
+	dfs(0, 0, 0);
+	if (min_ans == 987654321) min_ans = -1;
+	cout << min_ans;
+	return 0;
 }
