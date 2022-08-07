1a0d787506aeeaae057c377ecf1af39b4f7a3507
Guest <guest-joell5@h21460.sgb.skir.kr>
2022-08-07 20:06:13 +0900

    TODO: 12869

---
+++ 12869/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
+using namespace std;
+int N;
+int hp[3];
+int main()
+{
+    cin >> N;
+    for(int i = 0; i<N; i++)
+    {
+        cin >> hp[i];
+    }
+
+
+    return 0;
+}
