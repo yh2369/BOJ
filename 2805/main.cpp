4f2b00e6cbf587c04932616e66f0d0fa2369c8f3
Guest <guest-il0itj@h27247.sgb.skir.kr>
2022-08-15 00:54:08 +0900

    TODO: 2805 binary search timeout

--- 2805/main.cpp
+++ 2805/main.cpp
@@ -9,6 +9,8 @@ long long N; // 나무의 수
 long long M; // 가져가려는 나무의 길이
 
 void printV(vector<long long>& v);
+long long cut_tree(vector<long long>& height_arr, long long height);
+
 void printV(vector<long long>& v)
 {
     for(int i = 0; i<v.size(); i++)
@@ -18,26 +20,33 @@ void printV(vector<long long>& v)
     cout << "\n\n";
 }
 
+long long cut_tree(vector<long long>& height_arr, long long height)
+{
+    int n= height_arr.size();
+    long long ret = 0;
+    for(int i =0; i<n; i++)
+    {
+        long long delta = height_arr[i] - height;
+        if(delta > 0)
+        {
+            ret += delta;
+        }
+    }
+    return ret;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     cin >> N >> M;
-    map<long long, long long> height_cnt_table;
+    
     vector<long long> height_arr(N,0);
     long long max_height = 0;
     for(int i = 0; i< N; i++)
     {
         cin >> height_arr[i];
-        auto it = height_cnt_table.find(height_arr[i]);
-        if( it != height_cnt_table.end())
-        {
-            height_cnt_table[height_arr[i]]++;
-        }
-        else
-        {
-            height_cnt_table.insert(make_pair(height_arr[i], 1));
-        }
         max_height = max(max_height, height_arr[i]);
+        
     }
     
     if( N == 1)
@@ -47,38 +56,27 @@ int main()
     }
     sort(height_arr.begin(), height_arr.end());
 
-    /*
-    for(auto it = height_cnt_table.begin(); it != height_cnt_table.end(); it++)
+    long long a = 0; long long b = max_height;
+    long long height = 0;
+    while(true)
     {
-        auto key = it->first;
-        auto value = it->second;
-        cout << key <<": " << value << "\n";
-    }
-    cout << "\n";
-    */
-    long long h = max_height;
-    //cout << "h: " << h << "\n";
-    
-    while(h >= 0)
-    {
-        h--;
-        long long sum =0;
-        for(auto it = height_cnt_table.begin(); it != height_cnt_table.end(); it++)
+        height = (a+b)/2LL;
+        long long cut_tree_length = cut_tree(height_arr, height);
+        if(cut_tree_length < M)
         {
-            auto key = it->first;
-            auto value = it->second;
-            //cout << key <<": " << value << "\n";
-            if(key > h)
-            {
-                sum += (key - h) * value;
-            }
+            b = height;
         }
-        //cout << "sum: " << sum << "\n";
-        if(sum >= M)
+        else if(cut_tree_length >M)
+        {
+            a = height;
+        }
+        else
         {
             break;
         }
     }
-    cout << h << "\n";
+
+
+    cout << height << "\n";
     return 0;
 }
