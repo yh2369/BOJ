cdb0c18756659f591ff3bfd29ba227e7b8eef2b6
Guest <guest-jypiwh@h27247.sgb.skir.kr>
2022-08-21 19:48:05 +0900

    2839

---
+++ 2839/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+
+int dp[5004];
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin >> N;
+    for(int i = 0; i<=N; i++)
+    {
+        dp[i] = -1;
+    }
+    dp[3] = 1;
+    dp[5] = 1;
+    for(int i = 6; i<=N; i++)
+    {
+        if(dp[i-3] == -1 && dp[i-5] == -1){
+            continue;
+        }
+        else if(dp[i-3] != -1 && dp[i-5] == -1)
+        {
+            dp[i] = dp[i-3] + 1;
+        }
+        else if(dp[i-3] == -1 && dp[i-5] != -1)
+        {
+            dp[i] = dp[i-5] + 1;
+        }
+        else
+        {
+            dp[i] = min(dp[i-3], dp[i-5]) + 1;
+        }
+    }
+    cout << dp[N] << "\n";
+    return 0;
+}
